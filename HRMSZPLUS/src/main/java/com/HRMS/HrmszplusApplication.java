61e3e69668bde2be6335696267b5881e395681e9
Maddy <Maddy@DESKTOP-9DDNCV2>
2023-08-02 09:35:39 +0530

    Inital Structure Push

---
+++ HRMSZPLUS/src/main/java/com/HRMS/HrmszplusApplication.java
@@ -0,0 +1,13 @@
+package com.HRMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmszplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmszplusApplication.class, args);
+	}
+
+}
