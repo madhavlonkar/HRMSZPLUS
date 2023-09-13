35b635b9e3237905bee0ce4877a74a7360216418
Maddy <Maddy@DESKTOP-9DDNCV2>
2023-09-13 18:58:23 +0530

    New Modules Added-
    1)Added Bank Module With Entire Flow(CRUD)
    2)Added Department Module With Entire Flow(CURD)
    3)Added Designation Module With Entire Flow(CRUD)

--- HRMSZPLUS/src/main/java/com/HRMS/service/DesignationService.java
+++ HRMSZPLUS/src/main/java/com/HRMS/service/DesignationService.java
@@ -7,4 +7,8 @@ import com.HRMS.model.DesignationMaster;
 public interface DesignationService {
 
 	List<DesignationMaster> findAllDesignations();
+	DesignationMaster addDesignation(DesignationMaster designation);
+	DesignationMaster findById(int id);
+	DesignationMaster updateDesignation(DesignationMaster designation,int id);
+	void deleteDesignation(int id);
 }
